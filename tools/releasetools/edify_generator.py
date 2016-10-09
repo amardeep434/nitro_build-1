c29ead2c04b000e70532b1b00798201d9c5f5e80
Adin Kwok <adin.kwok@gmail.com>
2016-10-09 14:59:13 -0700

    Revert "Allow override of device asserts, including multi-device support."
    This reverts commit 410e968e237c4c6a81cc26e77ff68a61edc267d6.
    
    Conflicts:
    	core/Makefile
    	tools/releasetools/ota_from_target_files

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -132,11 +132,10 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = (' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
-                         % (i, i) for i in device.split(",")]) + ' || '
+    cmd = ('getprop("ro.product.device") == "%s" || '
            'abort("E%d: This package is for \\"%s\\" devices; '
            'this is a \\"" + getprop("ro.product.device") + "\\".");') % (
-               common.ErrorCode.DEVICE_MISMATCH, device)
+               device, common.ErrorCode.DEVICE_MISMATCH, device)
     self.script.append(cmd)
 
   def AssertSomeBootloader(self, *bootloaders):
